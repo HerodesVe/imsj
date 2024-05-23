4be9f06951f203e7f87e449ea3d5243139e7febb
CarlosPrad <cprado@sempiterno-group.com>
2024-05-23 15:53:38 -0400

    chore: Update Sidebar and Register components

--- src/pages/auth/Register/Register.tsx
+++ src/pages/auth/Register/Register.tsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
-import { toast } from 'react-toastify';
-import styles from './Register.module.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
+import { toast } from "react-toastify";
+import styles from "./Register.module.css";
 import vector from "../../../assets/vectorLeft.svg";
-import usePostRequest from '../../../hook/usePostRequest';
+import usePostRequest from "../../../hook/usePostRequest";
+import Modal from "../../../components/Modal/Modal";
 
 const Register: React.FC = () => {
   const navigate = useNavigate();
-  const { data, error, loading, postRequest } = usePostRequest('/register');
+  const { data, error, loading, postRequest } = usePostRequest("/register");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: ''
+    name: "",
+    email: "",
+    password: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -45,7 +47,11 @@ const Register: React.FC = () => {
     await postRequest(formData);
     if (!error) {
       toast.success("Registro exitoso");
-      navigate('/dashboard'); // Redirige a la página del dashboard después de registrarse
+      setIsModalOpen(true);
+      setTimeout(() => {
+        setIsModalOpen(false);
+        navigate("/");
+      }, 3000); // Cierra el modal y redirige después de 3 segundos
     }
   };
 
@@ -56,42 +62,28 @@ const Register: React.FC = () => {
         <form className={styles.form} onSubmit={handleRegister}>
           <div className={styles.inputGroup}>
             <FaUser className={styles.icon} />
-            <input 
-              type="text" 
-              name="name" 
-              placeholder="Nombre de Usuario" 
-              value={formData.name} 
-              onChange={handleChange} 
-            />
+            <input type="text" name="name" placeholder="Nombre de Usuario" value={formData.name} onChange={handleChange} />
           </div>
           <div className={styles.inputGroup}>
             <FaEnvelope className={styles.icon} />
-            <input 
-              type="email" 
-              name="email" 
-              placeholder="Correo Electrónico" 
-              value={formData.email} 
-              onChange={handleChange} 
-            />
+            <input type="email" name="email" placeholder="Correo Electrónico" value={formData.email} onChange={handleChange} />
           </div>
           <div className={styles.inputGroup}>
             <FaLock className={styles.icon} />
-            <input 
-              type="password" 
-              name="password" 
-              placeholder="Contraseña" 
-              value={formData.password} 
-              onChange={handleChange} 
-            />
+            <input type="password" name="password" placeholder="Contraseña" value={formData.password} onChange={handleChange} />
           </div>
           <button className={styles.registerButton} type="submit" disabled={loading}>
-            {loading ? 'Registrando...' : 'Registrar'}
+            {loading ? "Registrando..." : "Registrar"}
           </button>
         </form>
       </div>
       <div className={styles.vectorContainer}>
         <img src={vector} alt="Vectores de Fondo" />
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h2>Registro Exitoso</h2>
+        <p>Su registro está en revisión.</p>
+      </Modal>
     </div>
   );
 };
