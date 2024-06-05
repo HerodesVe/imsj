ebb4cf652ec48383150b36d6766edddf9e2914af
Heroi <heroiulacior@gmail.com>
2024-06-05 12:16:29 -0500

    fix route

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -61,7 +61,7 @@ const Dashboard = () => {
     setIsLoading(true); // Iniciar el loader
     try {
       const response = await axios.post(
-        `http://192.168.0.75:3000//uruguay/sendSoapRequest`,
+        `http://192.168.0.75:3000/uruguay/sendSoapRequest`,
         filter,
         {
           headers: {
